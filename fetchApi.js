d85a7682ac82107c59f535a1b6d62343cb3bbc80
hercules <aashish2535@gmail.com>
2023-05-16 23:31:50 +0530

    complete dummy json dynamic table

---
+++ fetchApi.js
@@ -0,0 +1,24 @@
+fetch('https://dummyjson.com/products')
+    .then(response => response.json())
+    .then(data => {
+        const products = data.products;
+        const tableBody = document.querySelector('#productTable tbody');
+
+        products.forEach(product => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <td>${product.id}</td>
+            <td>${product.title}</td>
+            <td>${product.description}</td>
+            <td>${product.price}</td>
+            <td>${product.discountPercentage}</td>
+            <td>${product.rating}</td>
+            <td>${product.stock}</td>
+            <td>${product.brand}</td>
+            <td>${product.category}</td>
+            <td><img src="${product.thumbnail}" alt="Thumbnail" width="50"></td>
+          `;
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error(error));
